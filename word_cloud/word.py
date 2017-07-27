4bb0f5ac966048af4a41cee6957f3f4b59d67ed3
yang <976026187@qq.com>
2017-07-27 16:57:04 +0800

    wordcloud实现词云

---
+++ word_cloud/word.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import codecs
+from wordcloud import WordCloud
+
+#根据已经分好词的文本写出词云
+__author__ = 'YangXiangjie'
+
+text = codecs.open(r'font/love.txt', 'r', encoding='utf-8').read()
+#匹配的图片形状
+mask = np.array(Image.open('image/love.jpg'))
+wc = WordCloud()
+wc = WordCloud(background_color='white', max_words=2000, mask=mask, font_path='叶立群几何体.ttf',
+               width=800, height=400)
+wc.generate(text)
+plt.imshow(wc)
+plt.axis('off')
+plt.show()
+wc.to_file('save.png')
